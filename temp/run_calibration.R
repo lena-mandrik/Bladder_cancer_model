275902a146219d4a62ab418d684cf56589beffc0
Olena Mandrik <o.mandrik@sheffield.ac.uk>
2022-11-23 17:19:50 +0000

    Plots for the targets

--- temp/run_calibration.R
+++ temp/run_calibration.R
@@ -1,4 +1,10 @@
 # This is the code to run calibration either with a random approach or a Bayesian way
+# Date: 23/11/22
+
+
+# Read all functions from all scripts within the R calibration folder
+
+sapply(list.files("R calibration",full.names=T), source)
 
 # Select the type of the calibration
 
@@ -10,32 +16,19 @@ Cohort_m <- sum(population[,"sex"]==1)
 Cohort_f <- sum(population[,"sex"]==0)
 
 
-# Extract the calibration targets
-Targets <- as.matrix(read.table("Data/Calibration_targets.txt", header = TRUE, row.names=1)) # Load targets by 5-years
-
+# Extract the calibration targets and calculate the confidence interval
+# The incidence is already converted per 1 person alive
+Targets <- read.table("Data/Calibration_targets.txt", header = TRUE, row.names=1) # Load targets by 5-years
 Targets <- f.targets.per.alive.long(Targets, start_age=30, end_age =95, n_by=5) #Linearly extrapolate between the 5-year bands
 
 n_targets <- ncol(Targets) -1 # Exclude Age column
 v.target_names <- colnames(Targets)[-1] # Exclude Age column
-
 CI_targets <- f.CI.calc(Targets)
 
-  
 
-  
 # Visualize the target
-targets_pt = ggplot(data = lst_targets$Surv,
-                    aes(x = time,
-                        y = value)) +
-  geom_errorbar(aes(ymin = lb, ymax = ub)) +
-  geom_point() +
-  theme(
-    panel.border = element_rect(fill = NA, color = 'black')
-  ) + 
-  labs(title = "Calibration target",
-       x = "Time",
-       y = "Proportion survived")
-targets_pt
+f.plot.target(Targets, CI_targets)
+
 
 # Define parameters to calibrate
 Calibr_parameters <- Params[c("P.onset", "P.onset_age", "P.onset_sex", "P.sympt.diag_LGBC", "P.sympt.diag_A_HGBC",
